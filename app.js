4c74faf8b53179b1b8d9445b42476e8b963bfe6b
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-04-14 16:42:51 -0400

    npm

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
 
-const fs = require('fs');
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
-const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const [name, github] = profileDataArgs;
 
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
