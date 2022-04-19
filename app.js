a441d6b80c5cab55d6b0ed5162d5a59c5d0c915c
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-04-18 22:06:58 -0400

    questions

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const inquirer = require('inquirer');
 
-inquirer
-.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?'
-  }
-])
-.then(answers => console.log(answers));
-
 
 // const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template');
 
-// const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-
-
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
+// fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw err;
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
